4ac0c6e3f32f118f586c8ea3b026429b09aae7e3
abhiy12 <102888992+abhiy12@users.noreply.github.com>
2026-07-27 20:05:02 +0530

    Update environment.ts

--- cms/frontend/src/environments/environment.ts
+++ cms/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api',
-  socketUrl: 'http://localhost:5000',
-  googleMapsApiKey: 'AIzaSyC36N_hStx1MjSaeYCUX4NJZvjQv09jQZY'
+  production: true,
+  apiUrl: 'https://complaint-management-system-1-15l5.onrender.com/api',
+  socketUrl: 'https://complaint-management-system-1-15l5.onrender.com',
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
 };

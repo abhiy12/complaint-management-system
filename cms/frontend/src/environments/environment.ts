717dcf0ff70dc6cd41398534174cde74b3227f0e
abhiy12 <102888992+abhiy12@users.noreply.github.com>
2026-07-29 16:13:30 +0530

    Remove Google Maps API key from environment file

--- cms/frontend/src/environments/environment.ts
+++ cms/frontend/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cms-backend-nlcc.onrender.com/api',
-  socketUrl: 'https://cms-backend-nlcc.onrender.com',
-  googleMapsApiKey: 'AIzaSyC36N_hStx1MjSaeYCUX4NJZvjQv09jQZY'
+  socketUrl: 'https://cms-backend-nlcc.onrender.com'
 };
-

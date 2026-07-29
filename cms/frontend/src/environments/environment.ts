04db191ec50057311b8fbb251cc0ad39913a18b0
abhiy12 <102888992+abhiy12@users.noreply.github.com>
2026-07-29 16:15:51 +0530

    Update environment.ts

--- cms/frontend/src/environments/environment.ts
+++ cms/frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cms-backend-nlcc.onrender.com/api',
-  socketUrl: 'https://cms-backend-nlcc.onrender.com'
+  socketUrl: 'https://cms-backend-nlcc.onrender.com',
+  googleMapsApiKey: 'AIzaSyC36N_hStx1MjSaeYCUX4NJZvjQv09jQZY'
 };
